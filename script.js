7fd5096ae24c31074032b781c21661be7b75d7c1
Nguyễn Huỳnh Hữu Nhâm <nhamb2005809@student.ctu.edu.vn>
2022-12-15 10:13:01 +0700

    first

---
+++ script.js
@@ -0,0 +1,253 @@
+
+var itemlist = {
+    "sp1": {
+        "name": "AIR-KING",
+        "price": 180065500,
+        "photo": "image/sanpham/rolex/rolex air-king.jpg"
+    },
+    "sp2": {
+        "name": "GMT-MASTER II",
+        "price": 372992500,
+        "photo": "image/sanpham/rolex/Gmt master II.jpg"
+    },
+    "sp3": {
+        "name": "DAY-DATE 40",
+        "price": 243500600,
+        "photo": "image/sanpham/rolex/day date 40.jpg"
+    },
+    "sp4": {
+        "name": "OYSTER PERPETUAL 41",
+        "price": 149197000,
+        "photo": "image/sanpham/rolex/oyster perpetual 41.jpg"
+    },
+    "sp5": {
+        "name": "COSMOGRAPH DAYTONA",
+        "price": 856597000,
+        "photo": "image/sanpham/rolex/COSMOGRAPH DAYTONA.jpg"
+    },
+    "sp6": {
+        "name": "LADY-DATEJUST",
+        "price": 913189000,
+        "photo": "image/sanpham/rolex/LADY-DATEJUST.jpg"
+    },
+    "sp7": {
+        "name": "YACHT-MASTER 42",
+        "price": 694538000,
+        "photo": "image/sanpham/rolex/YACHT-MASTER 42.jpg"
+    },
+    "sp8": {
+        "name": "DATEJUST 36",
+        "price": 293249000,
+        "photo": "image/sanpham/rolex/datejust 36.jpg"
+    },
+    "sp9": {
+        "name": "SUBMARINER",
+        "price": 218650500,
+        "photo": "image/sanpham/rolex/SUBMARINER.jpg"
+    },
+    "sp10": {
+        "name": "SEA-DWELLER",
+        "price": 416722500,
+        "photo": "image/sanpham/rolex/SEA-DWELLER.jpg"
+    },
+    "sp11": {
+        "name": "EXPLORER",
+        "price": 272670500,
+        "photo": "image/sanpham/rolex/EXPLORER.jpg"
+    },
+    "sp12": {
+        "name": "SKY-DWELLER",
+        "price": 980070000,
+        "photo": "image/sanpham/rolex/SKY-DWELLER.jpg"
+    },
+    "sp13": {
+        "name": "GLOBEMASTER",
+        "price": 956000000,
+        "photo": "image/sanpham/omega/GLOBEMASTER.png"
+    },
+    "sp14": {
+        "name": "CONSTELLATION",
+        "price": 416722500,
+        "photo": "image/sanpham/omega/CONSTELLATION.png"
+    },
+    "sp15": {
+        "name": "DIVER 300M",
+        "price": 416722500,
+        "photo": "image/sanpham/omega/DIVER 300M.png"
+    },
+    "sp16": {
+        "name": "PLANET OCEAN 600M",
+        "price": 272670500,
+        "photo": "image/sanpham/omega/PLANET OCEAN 600M.png"
+    },
+    "sp17": {
+        "name": "AQUA TERRA 150M",
+        "price": 856597000,
+        "photo": "image/sanpham/omega/AQUA TERRA 150M.png"
+    },
+    "sp18": {
+        "name": "PLOPROF 1200M",
+        "price": 913189000,
+        "photo": "image/sanpham/omega/PLOPROF 1200M.png"
+    },
+    "sp19": {
+        "name": "RACING",
+        "price": 694538000,
+        "photo": "image/sanpham/omega/RACING.png"
+    },
+    "sp20": {
+        "name": "SPEEDMASTER '57",
+        "price": 293249000,
+        "photo": "image/sanpham/omega/SPEEDMASTER '57.png"
+    },
+    "sp21": {
+        "name": "TRÉSOR",
+        "price": 218650500,
+        "photo": "image/sanpham/omega/TRÉSOR.png"
+    },
+    "sp22": {
+        "name": "AQUA TERRA 150M",
+        "price": 416722500,
+        "photo": "image/sanpham/omega/AQUA TERRA 150M lady.png"
+    },
+    "sp23": {
+        "name": "LADYMATIC",
+        "price": 272670500,
+        "photo": "image/sanpham/omega/LADYMATIC.png"
+    },
+    "sp24": {
+        "name": "PRESTIGE",
+        "price": 980070000,
+        "photo": "image/sanpham/omega/PRESTIGE lady.png"
+    },
+    "sp25": {
+        "name": "UNICO TITANIUM",
+        "price": 180065500,
+        "photo": "image/sanpham/hublot/UNICO TITANIUM.png"
+    },
+    "sp26": {
+        "name": "UNICO ALL BLACK",
+        "price": 372992500,
+        "photo": "image/sanpham/hublot/UNICO ALL BLACK.png"
+    },
+    "sp27": {
+        "name": "E FIFA WORLD CUP QATAR 2022™",
+        "price": 243500600,
+        "photo": "image/sanpham/hublot/E FIFA WORLD CUP QATAR 2022™.png"
+    },
+    "sp28": {
+        "name": "TOURBILLON SAMUEL ROSS",
+        "price": 149197000,
+        "photo": "image/sanpham/hublot/TOURBILLON SAMUEL ROSS.png"
+    },
+    "sp29": {
+        "name": "ONE CLICK STEEL DIAMONDS",
+        "price": 856597000,
+        "photo": "image/sanpham/hublot/ONE CLICK STEEL DIAMONDS.png"
+    },
+    "sp30": {
+        "name": "ONE CLICK KING GOLD DIAMONDS",
+        "price": 913189000,
+        "photo": "image/sanpham/hublot/ONE CLICK KING GOLD DIAMONDS.png"
+    },
+    "sp31": {
+        "name": "TAKASHI MURAKAMI SAPPHIRE RAINBOW",
+        "price": 694538000,
+        "photo": "image/sanpham/hublot/TAKASHI MURAKAMI SAPPHIRE RAINBOW.png"
+    },
+    "sp32": {
+        "name": "AEROFUSION MOONPHASE TITANIUM",
+        "price": 293249000,
+        "photo": "image/sanpham/hublot/AEROFUSION MOONPHASE TITANIUM.png"
+    },
+    "sp33": {
+        "name": "AEROFUSION MOONPHASE KING GOLD PAVÉ",
+        "price": 218650500,
+        "photo": "image/sanpham/hublot/AEROFUSION MOONPHASE KING GOLD PAVÉ.png"
+    },
+    "sp34": {
+        "name": "TOURBILLON BI-AXIS 3D CARBON",
+        "price": 416722500,
+        "photo": "image/sanpham/hublot/TOURBILLON BI-AXIS 3D CARBON.png"
+    },
+    "sp35": {
+        "name": "CHRONOGRAPH TITANIUM BLUE",
+        "price": 272670500,
+        "photo": "image/sanpham/hublot/CHRONOGRAPH TITANIUM BLUE.png"
+    },
+    "sp36": {
+        "name": "CHRONOGRAPH TITANIUM GREEN",
+        "price": 980070000,
+        "photo": "image/sanpham/hublot/CHRONOGRAPH TITANIUM GREEN.png"
+    },
+};
+
+function addcard(id) {
+
+    current = parseInt(localStorage.getItem(id));
+    if ((typeof localStorage[id] === "undefined")) {
+        localStorage.setItem(id, 1);
+        alert('Bạn đã thêm 1 đồng hồ' + " " + itemlist[id].name + " vào giỏ hàng");
+    } else {
+        localStorage.setItem(id, current + 1);
+        alert('Bạn đã thêm 1 đồng hồ' + ' ' + itemlist[id].name + ' thành công');
+    }
+}
+function showcart() {
+    totalpretax = 0;
+
+    for (let i = 0; i < localStorage.length; i++) {
+        item = itemlist[localStorage.key(i)];
+        photo = item.photo;
+        name1 = item.name;
+        price = item.price;
+        number = localStorage.getItem(localStorage.key(i));
+
+        var tr1 = document.createElement("tr");
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+        var td4 = document.createElement("td");
+        var td5 = document.createElement("td");
+        var td6 = document.createElement("td");
+        td1.innerHTML = "<img src='" + photo + "' width='100px'/>";
+        td2.innerHTML = name1;
+        td3.innerHTML = new Intl.NumberFormat('vi-VN').format(price) + " VND";
+        td4.innerHTML = number;
+        td5.innerHTML = new Intl.NumberFormat('vi-VN').format(number * price) + " VND";
+        td6.innerHTML = "<i class='fa-solid fa-x'  onclick='removecart(\"" + localStorage.key(i) + "\")'></i>";
+        tr1.appendChild(td1);
+        tr1.appendChild(td2);
+        tr1.appendChild(td3);
+        tr1.appendChild(td4);
+        tr1.appendChild(td5);
+        tr1.appendChild(td6);
+        document.getElementById("list-product-tbody").appendChild(tr1);
+
+        totalpretax = totalpretax + (number * price);
+    }
+    document.getElementById("dh-tt").innerHTML = new Intl.NumberFormat('vi-VN').format(totalpretax) + " VND";
+
+
+
+}
+
+
+function removecart(id) {
+    window.localStorage.removeItem(id);
+    location.reload();
+
+}
+
+window.onstorage = () => {
+    location.reload();
+};
+
+
+
+function cartcheck() {
+    var number =localStorage.length;
+    if (number == 0) {
+        document.getElementById('cart-check-yes').style.display = "none";
+    } else { document.getElementById('cart-check-no').style.display = "none";}
+}
